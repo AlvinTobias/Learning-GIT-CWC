fd7e624c7d8d2342a845fde717bc69cb54c4f60f
Alvin <alvintob@gmail.com>
2022-01-06 16:19:17 -0500

    Initial Commit

---
+++ Alvin-GIT-Test/Alvin_GIT_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Alvin_GIT_TestApp.swift
+//  Alvin-GIT-Test
+//
+//  Created by Jainy Alvin on 1/6/22.
+//
+
+import SwiftUI
+
+@main
+struct Alvin_GIT_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
