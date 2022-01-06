f71e6b58f725e9c2740e27f2571324a155f3e97f
Alvin <alvintob@gmail.com>
2022-01-06 16:26:55 -0500

    Changes the CWC to Crew

--- Alvin-GIT-Test/ContentView.swift
+++ Alvin-GIT-Test/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("I am doing CWC")
+        Text("This is another Change crew..!")
             .padding()
     }
 }
